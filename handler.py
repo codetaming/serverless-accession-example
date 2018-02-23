7a68f20a0a6113359d851d8826e754c74af06128
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-02-23 11:51:23 +0000

    get sha1 and uuid

--- handler.py
+++ handler.py
@@ -2,6 +2,8 @@ import json
 from boto3 import resource
 import os
 import decimal
+import uuid
+import hashlib
 
 
 class DecimalEncoder(json.JSONEncoder):
@@ -30,6 +32,36 @@ def get_accession(event, context):
 
     body = {
         "accession": "HCA-" + str(response['Attributes'][field_name])
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+
+def get_sha1(event, context):
+    body = json.loads(event["body"])
+    hash_result = hashlib.sha1(str(body).encode('utf-8')).hexdigest()
+    print(hash_result)
+
+    body = {
+        "sha1": str(hash_result)
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+
+def get_uuid(event, context):
+    body = {
+        "uuid": str(uuid.uuid4())
     }
 
     response = {
